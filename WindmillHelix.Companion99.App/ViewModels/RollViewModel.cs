c3034616dbf2785ee8cd06726b79673f169463ac
Edward Sampson <bitbucket@sampson.tv>
2025-04-15 02:12:43 -0500

    Rolls tab

---
+++ WindmillHelix.Companion99.App/ViewModels/RollViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WindmillHelix.Companion99.App.ViewModels;
+
+namespace WindmillHelix.Companion99.App.ViewModels
+{
+    public class RollViewModel
+    {
+        public RollSetViewModel RollSet { get; set; }
+
+        public string CharacterName { get; set; }
+
+        public DateTime RollTime { get; set; }
+
+        public int Value { get; set; }
+
+        public string TimeAfterFirstRoll
+        {
+            get
+            {
+                var span = RollTime - RollSet.FirstRoll;
+                var seconds = Math.Round(span.TotalSeconds, 0);
+                return $"{seconds}s";
+            }
+        }
+    }
+}
